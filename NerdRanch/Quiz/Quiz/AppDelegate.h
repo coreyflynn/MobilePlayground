661781eb7c025434796fc8915f408ed596de5e84
Corey Flynn <flynn.corey@gmail.com>
2013-02-24 15:00:57 -0500

    added nerd ranch quiz project

---
+++ NerdRanch/Quiz/Quiz/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Quiz
+//
+//  Created by Corey Flynn on 2/24/13.
+//  Copyright (c) 2013 Corey Flynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ NerdRanch/Quiz/Quiz/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Corey Flynn on 2/24/13.
+//  Copyright (c) 2013 Corey Flynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
