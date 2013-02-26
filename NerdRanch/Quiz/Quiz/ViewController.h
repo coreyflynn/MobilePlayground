55176eda0909ec5af2d06e57e89a2a5b670cf844
Corey Flynn <flynn.corey@gmail.com>
2013-02-26 09:50:59 -0500

    UI and first functional prototype for NerdRanch Quiz

--- NerdRanch/Quiz/Quiz/ViewController.h
+++ NerdRanch/Quiz/Quiz/ViewController.h
@@ -9,5 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int currentQuestionIndex;
+    
+    //    the model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    //    the view objects
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+
+-(IBAction)showQuestion:(id)sender;
+-(IBAction)showAnswer:(id)sender;
 
 @end
--- NerdRanch/Quiz/Quiz/ViewController.m
+++ NerdRanch/Quiz/Quiz/ViewController.m
@@ -13,17 +13,56 @@
 @end
 
 @implementation ViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+-(id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    // call the init method implimented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // create two arrays and make the pointers point to them
+        questions = [[NSMutableArray alloc]init];
+        answers = [[NSMutableArray alloc]init];
+        
+        // add questions and answers to the arrays
+        [questions addObject:@"what is 7 + 7?"];
+        [answers addObject:@"14"];
+        
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers addObject:@"Montpelier"];
+        
+        [questions addObject:@"From what is cognac made?"];
+        [answers addObject:@"Grapes"];
+    }
+    
+    // return the address of the new object
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(IBAction)showQuestion:(id)sender {
+    // step to the next question
+    currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (currentQuestionIndex == [questions count]) {
+        // go back to the first question
+        currentQuestionIndex = 0;
+    }
+    
+    // get the string at that index in the question array and display it
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    [questionField setText:question];
+    
+    //log the string to the console
+    NSLog(@"displaying quesion: %@", question);
+    
+    // clear the answer field
+    [answerField setText:@"???"];
+}
+
+-(IBAction)showAnswer:(id)sender {
+    //what is the answer to the current question
+    NSString *answer = [answers objectAtIndex:currentQuestionIndex];
+    
+    //display it in the answer field
+    [answerField setText:answer];
 }
 
 @end
